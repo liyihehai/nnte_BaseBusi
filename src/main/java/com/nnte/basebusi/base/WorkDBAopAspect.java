567478c5887de258ce2f878024755f3a63469ed1
liyihehai <liyi_hehai@sina.com>
2020-11-08 12:06:44 +0800

    取得数据连接不依赖参数

--- src/main/java/com/nnte/basebusi/base/WorkDBAopAspect.java
+++ src/main/java/com/nnte/basebusi/base/WorkDBAopAspect.java
@@ -2,6 +2,7 @@ package com.nnte.basebusi.base;
 
 import com.nnte.basebusi.annotation.DBSrcTranc;
 import com.nnte.framework.base.BaseNnte;
+import com.nnte.framework.base.BaseService;
 import com.nnte.framework.base.ConnSqlSessionFactory;
 import com.nnte.framework.base.DynamicDatabaseSourceHolder;
 import org.aspectj.lang.JoinPoint;
@@ -20,27 +21,37 @@ import java.util.Map;
 @Aspect
 @Component
 public class WorkDBAopAspect {
+    /*
     @Autowired
     private DynamicDatabaseSourceHolder dynamicDSHolder;
-
+    */
     /*切点条件：
      * 1:组件必须有@WorkDBAspect注解，
      * 2:方法必须有@DBSrcTranc注解，
-     * 3:第一个参数必须是Map<String,Object> paramMap，
+     * 3:第一个参数必须是Map<String,Object> paramMap， (去除)
      * 4:返回值必须是Map<String,Object>
      */
+    /*
     @Pointcut("@target(com.nnte.framework.annotation.WorkDBAspect) && @annotation(com.nnte.basebusi.annotation.DBSrcTranc) && args(pMap,..) && execution(public java.util.Map<String,Object> *(..))")
     public void WorkDBAopPointCut(Map<String,Object> pMap){ }
-
     @Around(value = "WorkDBAopPointCut(pMap)")
     public Object doAround(ProceedingJoinPoint pjp, Map<String,Object> pMap) throws Throwable {
-        Connection conn=null;
+    */
+    @Pointcut("@target(com.nnte.framework.annotation.WorkDBAspect) && @annotation(com.nnte.basebusi.annotation.DBSrcTranc) && execution(public java.util.Map<String,Object> *(..))")
+    public void WorkDBAopPointCut(){ }
+
+    @Around(value = "WorkDBAopPointCut()")
+    public Object doAround(ProceedingJoinPoint pjp) throws Throwable {
+
         Map<String,Object> ret = BaseNnte.newMapRetObj();
         try {
             Method method = getMethod(pjp);
             DBSrcTranc dbSrcTranc = method.getAnnotation(DBSrcTranc.class);
             String value = dbSrcTranc.value();
             boolean autocommit=dbSrcTranc.autocommit();
+            BaseNnte.outConsoleLog("WorkDBAopPointCut dataSrcName="+value+" start ...");
+            BaseService.setThreadLocalCSSF(value,autocommit);
+            /*
             DynamicDatabaseSourceHolder.dbsourceSqlSessionFactory dbsrcSSF=dynamicDSHolder.getDBsrcSSF(value);
             if (dbsrcSSF==null)
             {
@@ -57,10 +68,10 @@ public class WorkDBAopAspect {
                 return ret;
             }
             conn.setAutoCommit(autocommit);
+            ConnSqlSessionFactory cssf=new ConnSqlSessionFactory(conn,dbsrcSSF.getSqlSessionFactory());
             if (null != pMap) {
-                ConnSqlSessionFactory cssf=new ConnSqlSessionFactory(conn,dbsrcSSF.getSqlSessionFactory());
                 pMap.put("ConnSqlSessionFactory",cssf);
-            }
+            }*/
             ret =  (Map<String,Object>)pjp.proceed();
             return ret;
         } catch (Throwable e) {
@@ -69,16 +80,8 @@ public class WorkDBAopAspect {
             e.printStackTrace();
             return ret;
         } finally {
-            if (conn!=null && !conn.isClosed())
-            {
-                if (!conn.getAutoCommit()) {
-                    if (ret!=null && BaseNnte.getRetSuc(ret))
-                        conn.commit();
-                    else
-                        conn.rollback();
-                }
-                conn.close();
-            }
+            BaseService.removeThreadLocalCSSF(ret);
+            BaseNnte.outConsoleLog("WorkDBAopPointCut finally ...");
         }
     }
 
