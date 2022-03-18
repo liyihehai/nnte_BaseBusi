edb20d9cb26445428069fb5e084e15be7b0c4899
liyihehai <liyi_hehai@sina.com>
2022-03-18 20:07:32 +0800

    优化日志功能

--- src/main/java/com/nnte/basebusi/base/BeanListenerProcessor.java
+++ src/main/java/com/nnte/basebusi/base/BeanListenerProcessor.java
@@ -10,6 +10,9 @@ import org.springframework.stereotype.Component;
 @Component
 @BusiLogAttr(BaseBusi.Logger_Name)
 public class BeanListenerProcessor extends BaseNnte implements BeanPostProcessor {
+    public BeanListenerProcessor(){
+        setFrame_loggerName(BaseBusi.Logger_Name);
+    }
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
         return bean;
@@ -21,13 +24,14 @@ public class BeanListenerProcessor extends BaseNnte implements BeanPostProcessor
         String className=clazz.getSimpleName();
         outLogInfo("设置组件[" + className + "]创建完成!");
         if (bean instanceof BaseNnte) {
+            BaseNnte BaseNntebean = (BaseNnte) bean;
             BusiLogAttr logAttr = bean.getClass().getAnnotation(BusiLogAttr.class);
             if (logAttr != null) {
                 String loggerName = logAttr.value();
-                BaseNnte BaseNntebean = (BaseNnte) bean;
                 BaseNntebean.setFrame_loggerName(loggerName);
                 BaseNntebean.outLogInfo("设置组件[" + className + "]日志属性：" + logAttr.value());
-            }
+            }else
+                BaseNntebean.setFrame_loggerName(className);
         }
         return bean;
     }
